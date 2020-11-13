14ed147ccefe05aab276222da085c0ecd1a691bd
James Peralta <jamesperalta@Jamess-MacBook-Pro.local>
2020-11-12 22:41:12 -0700

    Reverting Server

--- Mapping/2DMapping.py
+++ Mapping/2DMapping.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+import time
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -8,6 +9,7 @@ cap.set(4, 480)
 
 while True:
     success, img = cap.read()
+    start = time.monotonic()
     for barcode in decode(img):
         my_data = barcode.data.decode('utf-8')
         pts = np.array([barcode.polygon], np.int32)
@@ -16,6 +18,7 @@ while True:
         cv2.polylines(img, [pts], True, (255, 0, 255), 5)
         cv2.putText(img, my_data, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
                     0.9, (255, 0, 255), 2)
-
+    end = time.monotonic()
+    print((end - start) * 1000)
     cv2.imshow('Result', img)
     cv2.waitKey(1)
